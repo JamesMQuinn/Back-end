2262bd666751d3d11f192d30b737df5905a5e8cf
Chris Hedberg <Chris.Hedberg@chrobinson.com>
2016-08-23 14:43:40 -0500

    Removed Application Insights and added Log4Net.

--- src/ServiceStackServiceTemplate/Global.asax.cs
+++ src/ServiceStackServiceTemplate/Global.asax.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Web.Configuration;
 
 namespace ServiceStackServiceTemplate
 {
@@ -7,6 +6,18 @@ namespace ServiceStackServiceTemplate
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            // More information on Log4Net logging can be found here: http://wiki/dokuwiki/doku.php?id=arch:log4net_setup
+            
+            //set ServiceStack's LogFactory
+            ServiceStack.Logging.LogManager.LogFactory = new ServiceStack.Logging.Log4Net.Log4NetFactory();
+            
+            //set our LogFactory
+            CHRobinson.Enterprise.Logging.LogManager.LogFactory = new CHRobinson.Enterprise.Logging.Log4Net.Log4NetFactory(true);
+            
+            log4net.GlobalContext.Properties["AppName"] = "ServiceStackServiceLog4NetTemplate";
+            
+            log4net.Config.XmlConfigurator.Configure();
+
             new AppHost().Init();
         }
     }
