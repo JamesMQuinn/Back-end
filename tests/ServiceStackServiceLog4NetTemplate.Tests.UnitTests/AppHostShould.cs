575d0fabad2d479fbff319aacded453d46a91469
Chris Hedberg <Chris.Hedberg@chrobinson.com>
2016-08-25 16:13:48 -0500

    Added moq nuget reference and fixed codecoverage.runsettings.

--- tests/ServiceStackServiceLog4NetTemplate.Tests.UnitTests/AppHostShould.cs
+++ tests/ServiceStackServiceLog4NetTemplate.Tests.UnitTests/AppHostShould.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ServiceStack.Text;
+using Moq;
 
 namespace ServiceStackServiceLog4NetTemplate.Tests.UnitTests
 {
