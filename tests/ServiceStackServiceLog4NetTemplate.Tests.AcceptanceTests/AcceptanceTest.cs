64dcc7dfac43724af9338c172e7743a48f7962a7
Hayden Wade <Hayden.Wade@chrobinson.com>
2017-06-15 14:34:37 -0500

    downgraded servicestack version to 4.0.62

--- tests/ServiceStackServiceLog4NetTemplate.Tests.AcceptanceTests/AcceptanceTest.cs
+++ tests/ServiceStackServiceLog4NetTemplate.Tests.AcceptanceTests/AcceptanceTest.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CHR.ServiceStack.Plugins.Models.HealthCheckFeature;
+using ServiceStack;
 
 namespace ServiceStackServiceLog4NetTemplate.Tests.AcceptanceTests
 {
     [TestClass]
     public class AcceptanceTest
     {
+        private IJsonServiceClient _jsonClient;
+
         [TestInitialize]
         public void InitializeTestData()
         {
@@ -20,6 +24,43 @@ namespace ServiceStackServiceLog4NetTemplate.Tests.AcceptanceTests
                 Console.WriteLine($"{key}: '{ConfigurationManager.AppSettings[key]}'");
             }
             Console.WriteLine("--End AppSettings--");
+            _jsonClient = new JsonServiceClient(ConfigurationManager.AppSettings["ServiceUrl"]);
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _jsonClient.Dispose();
+        }
+        [TestMethod, TestCategory("AutomatedAcceptance")]
+        public void CheckServiceHealth_ShouldBeHealthy()
+        {
+            HealthCheckRequest request = new HealthCheckRequest();
+
+            Console.WriteLine($"Calling Health Check...");
+            HealthCheckResponse response = _jsonClient.Get(request);
+
+            if (response.AmIHealthy)
+            {
+                Console.WriteLine("Healthy Health Check Returned.");
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Console.WriteLine($"Found {response?.UnresolvedUrls?.Count} unresolved urls");
+                if (response?.UnresolvedUrls != null)
+                {
+                    foreach (var unresolvedUrl in response.UnresolvedUrls)
+                    {
+                        Console.WriteLine($"Url/DB that was unresolved: {unresolvedUrl}");
+                    }
+                    Assert.IsTrue(false);
+                }
+                else
+                {
+                    Console.WriteLine($"AmIHealthy is False, but UnResolved URLS are Null");
+                    Assert.IsTrue(false);
+                }
+            }
         }
     }
 }
